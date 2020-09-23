4b954a41c755cfc280acdad41fea7159b73bc911
Bobby Patel <bobbyrp@uci.edu>
2020-09-23 00:33:00 -0700

    Added React build to Node Project Folder

---
+++ build/precache-manifest.3308164121c73e6f3e59f7f7c03db80f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a21c3825f422ca566cc01957a149fb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e7219e1bab689ac08eb",
+    "url": "/static/css/main.f73f7164.chunk.css"
+  },
+  {
+    "revision": "5963b1fbd49199ff1727",
+    "url": "/static/js/2.de98669a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.de98669a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e7219e1bab689ac08eb",
+    "url": "/static/js/main.1482cded.chunk.js"
+  },
+  {
+    "revision": "4b4367ca4fbadb01e2d9",
+    "url": "/static/js/runtime-main.e5a25448.js"
+  }
+]);
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json())
 const cors = require('cors')
 app.use(cors())
 
-
+app.use(express.static('build'))
 
 
 
