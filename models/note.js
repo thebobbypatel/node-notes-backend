8756569c2c97ab4b9eae73674321b46c0fd523e7
Bobby Patel <bobbyrp@uci.edu>
2020-09-29 00:22:40 -0700

    Added Testing using Jest
    Also separated Database schema code and interaction code to model and controller folders.

--- models/note.js
+++ models/note.js
@@ -1,20 +1,15 @@
 const mongoose = require('mongoose')
-mongoose.set('useFindAndModify', false)
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
+  content: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
   important: Boolean,
 })
 
